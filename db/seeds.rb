504379460940437235683a65a469cea43dc4e7a1
apprentice1988 <zilongji@gmail.com>
2015-10-24 16:39:38 +0800

    finish seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,8 @@
 Project.create([
   { address: '昙华林', name: '听说昙华林以西曾是戈甲营，走，去看看', lat: 30.552127, lon: 114.309425, image: []},
-  { address: '青年大教堂', name: '到青年大教堂给Hackathon点赞', lat: 30.502299, lon: 114.406995, image: []},
-  { address: '红楼', name: '拾起1911的那段大历史', lat: 30.5516854 , lon: 114.3090414, image: []},
-  { address: '省博', name: '对， 这次去省博去找个人', lat: 30.5516854 , lon: 114.3090414, image: []},
-  { address: '黄鹤楼', name: '长江边的那块匾， 你还记得吗？', lat: 30.5516854 , lon: 114.3090414, image: []},
-  { address: '武汉大学', name: '看樱花， 不了， 去找那个诗人', lat: 30.5516854 , lon: 114.3090414, image: []}
+  { address: '青年大教堂', name: '到青年大教堂给Hackathon点赞', lat: 30.502404, lon: 114.407113, image: []},
+  { address: '红楼', name: '拾起1911的那段大历史', lat: 30.542217 , lon: 114.306489, image: []},
+  { address: '省博', name: '对， 这次去省博去找个人', lat: 30.561931 , lon: 114.365500, image: []},
+  { address: '黄鹤楼', name: '长江边的那块匾， 你还记得吗？', lat: 30.544636 , lon: 114.302537, image: []},
+  { address: '武汉大学', name: '看樱花， 不了， 去找那个诗人', lat: 30.533289 , lon: 114.358140, image: []}
 ]
