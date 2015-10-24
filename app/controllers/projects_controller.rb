dbf19d9f4af4d7a69e17fc3ff00d07405c41b34b
= <zilongji@gmail.com>
2015-10-25 00:00:20 +0800

    remove user

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -10,15 +10,15 @@ class ProjectsController < ApplicationController
 
   def start
     @project = Project.find(params[:id])
-    @user = User.first
+    # @user = User.first
     # @parti = Participation.find_or_initialize_by(project_id: @project.id, user_id: @user.id)
-    @parti = @user.participations.create(project_id: @project.id, status: 'doing')
+    @parti = Participations.create(project_id: @project.id, status: 'doing')
     render json: { success: true }
   end
 
   def checkin
     @project = Project.find(params[:id])
-    @user = User.first
+    # @user = User.first
     if @project.distance([params[:coordinates].try(&:first).to_f, params[:coordinates].try(&:last).to_f]) <= 2
       render json: { success: true, content: @project.puzzle.answer, hint: @project.puzzle.hint }
     else
@@ -28,9 +28,9 @@ class ProjectsController < ApplicationController
 
   def validate
     @project = Project.find(params[:id])
-    @user = User.first
+    # @user = User.first
     if @project.puzzle.answer == params[:answer]
-      @parti = @user.participations.find_by(project_id: @project.id)
+      @parti = Participation.find_by(project_id: @project.id)
       @parti.update(status: 'finished')
       render json: { success: true }
     else
