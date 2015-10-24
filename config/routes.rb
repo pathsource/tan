a74f926e5b46b4355e803623f0fbef591681c67e
= <zilongji@gmail.com>
2015-10-24 23:01:12 +0800

    add more http action to api

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,11 @@ Rails.application.routes.draw do
   resources :projects do
     member do
       post 'start'
+      get 'start'
       post 'checkin'
+      get 'checkin'
       post 'validate' 
+      get 'validate'
     end
   end
 end
