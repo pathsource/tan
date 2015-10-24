8a2a1cd04a36732e326f4f6b8ddb994ab3102dc4
= <zilongji@gmail.com>
2015-10-24 18:24:17 +0800

    fix spell bug

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,4 @@ Project.create([
   { address: '省博', name: '对， 这次去省博去找个人', lat: 30.561931 , lon: 114.365500, image: []},
   { address: '黄鹤楼', name: '长江边的那块匾， 你还记得吗？', lat: 30.544636 , lon: 114.302537, image: []},
   { address: '武汉大学', name: '看樱花， 不了， 去找那个诗人', lat: 30.533289 , lon: 114.358140, image: []}
-]
+])
