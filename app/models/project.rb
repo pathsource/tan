23e996afbc49ecf0479c52fac1848f3be194426a
xx <1490923109@qq.com>
2015-10-25 02:10:54 +0800

    when coordinates is null return 0 in distance method

--- app/models/project.rb
+++ app/models/project.rb
@@ -5,6 +5,7 @@ class Project < ActiveRecord::Base
 
   def distance(coordinates)
     coordinates ||= []
+    return 0 if coordinates.map(&:to_i) == [0, 0]
     num = Geocoder::Calculations.distance_between(coordinates, [lat.to_f, lon.to_f]).round(1)
     # "#{num} km"
   end
